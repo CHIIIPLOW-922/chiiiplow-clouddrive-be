980e176f33023436684b73fbdacdb3fa75c11de3
yangzhixiong <641484973@qq.com>
2024-12-05 16:51:11 +0800

    Init Back-End Program

---
+++ src/main/java/com/chiiiplow/clouddrive/CloudDriveApplication.java
@@ -0,0 +1,12 @@
+package com.chiiiplow.clouddrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDriveApplication.class);
+    }
+}
