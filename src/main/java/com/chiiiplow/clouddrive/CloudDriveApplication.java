7f138356e0122ef7c59239df10aee2cb0668b04e
yangzhixiong <641484973@qq.com>
2024-12-05 16:56:28 +0800

    Genshin Impact Launch!!!

--- src/main/java/com/chiiiplow/clouddrive/CloudDriveApplication.java
+++ src/main/java/com/chiiiplow/clouddrive/CloudDriveApplication.java
@@ -3,6 +3,12 @@ package com.chiiiplow.clouddrive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 网盘！启动！
+ *
+ * @author yangzhixiong
+ * @date 2024/12/05
+ */
 @SpringBootApplication
 public class CloudDriveApplication {
 
